e40564525938a49c95c075bfea80fc67c658ed24
Ivan.Barbaric <ivan.barbaric992@gmail.com>
2019-04-08 22:31:10 +0200

    front end finished

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,7 +34,7 @@ class Register extends React.Component {
                 name: this.state.Name
             })
         }).then(response => response.json()).then(user => {
-            if (user)
+            if (user.id)
             {
                 this.props.loadUser(user)
                 this.props.onRouteChange('home');
